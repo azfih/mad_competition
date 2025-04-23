7ee88b6a71af3029c6bc6fe766af4e2e73cb9073
Hifza Rafiq <hifzarafiq19@gmail.com>
2025-04-23 12:39:52 +0500

    Saved my changes to routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -5,7 +5,6 @@ import 'screens/splash_screen.dart';
 import 'screens/auth/login_screen.dart';
 import 'screens/auth/register_screen.dart';
 import 'screens/tutor/tutor_dashboard.dart';
-import 'screens/student/student_dashboard.dart';
 import 'screens/tutor/create_course_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
@@ -14,5 +13,9 @@ final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => const SplashScreen(),
   '/login': (context) => const LoginScreen(),
   '/register': (context) => const RegisterScreen(),
-
+  '/tutorDashboard': (context) {
+    final uid = ModalRoute.of(context)!.settings.arguments as String;
+    return TutorDashboard(tutorId: uid);
+  },
+  '/createCourse': (context) => const CreateCourseScreen(),
 };
